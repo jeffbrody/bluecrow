eb669747899a6a97a713db768359876a82936a85
Jeff Brody <jeff@bluecrowconsulting.com>
2012-12-31 12:19:58 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bluecrow::Application.config.secret_token = '1cea0041b256681ec310ace08e285b1384b2cbb2125e543be78d55c16985efd09ca2fb2025d7127889498bf48895cc83d6daa5f0356cd5c278a420d3b4c3208b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bluecrow::Application.config.session_store :cookie_store, key: '_bluecrow_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bluecrow::Application.config.session_store :active_record_store
