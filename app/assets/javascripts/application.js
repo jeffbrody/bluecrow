f0d71d91893c6211603535fdfd99e75373af5715
Jeff Brody <jeff@bluecrowconsulting.com>
2013-01-01 17:44:28 -0700

    Adding boilerplate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require modernizr.custom.17738.js
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
